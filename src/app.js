c480d1790760d3db86c06b5c17fa4b625d4a67a5
nXtremist <randomworkid@gmail.com>
2021-06-29 18:21:43 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,86 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const utils = require('./utils')
+
+const publicDir = path.join(__dirname, '../public')
+const viewsDir = path.join(__dirname, '../templates/views')
+const partialsDir = path.join(__dirname, '../templates/partials')
+
+const app = express()
+
+app.set('view engine', 'hbs')
+app.set('views', viewsDir)
+hbs.registerPartials(partialsDir)
+
+app.use(express.static(publicDir))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: "Weather",
+        name: "Abc"
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: "About",
+        name: "Abc"
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: "Help?",
+        name: "Abc",
+        message: "This be not helpful at all"
+    })
+})
+
+app.get('/weather', (req, res) => {
+    if (!req.query.address) return res.send({
+        error: "No address provided"
+    })
+
+    utils.geocode(req.query.address, (error, {
+        latitude,
+        longitude,
+        location
+    } = {}) => {
+        if (error) return res.send({
+            error
+        })
+
+        utils.forecast(latitude, longitude, (error, forecast) => {
+            if (error) return res.send({
+                error
+            })
+
+            res.send({
+                forecast,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        title: "Help 404",
+        name: "Abc",
+        message: "Help Not found"
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: "404",
+        name: "Abc",
+        message: "404<br>Not Found"
+    })
+})
+
+app.listen(4000, () => {
+    console.log('Runnin');
+})
---
+++ src/utils.js
@@ -0,0 +1,42 @@
+const request = require('request')
+
+if (process.env.RELEASE !== 'release')
+    require('dotenv').config()
+
+const geocode = (address, callback) => {
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(address)}.json?access_token=${process.env.GEOCODE_TOKEN}&limit=1`
+    request({
+        url,
+        json: true
+    }, (err, {
+        body
+    } = {}) => {
+        if (err) return callback('Unable to connect.', undefined)
+        if (body.message || body.features.length === 0) return callback('Unable to get the location.', undefined)
+        callback(undefined, {
+            latitude: body.features[0].center[1],
+            longitude: body.features[0].center[0],
+            location: body.features[0].place_name
+        })
+    })
+}
+
+const forecast = (latitude, longitude, callback) => {
+    const url = `http://api.weatherstack.com/current?access_key=${process.env.WEATHER_TOKEN}&query=${latitude},${longitude}`
+    request({
+        url,
+        json: true
+    }, (error, {
+        body
+    } = {}) => {
+        if (error) return callback('Unable to connect. Check your connection.', undefined);
+        if (body.indexOf('<html>') > -1) return callback('Unable to connect. Try again after sometime.', undefined);
+        if (body.error) return callback('Unable to find location.', undefined);
+        callback(undefined, `It is currently ${body.current.temperature} degrees. It is ${body.current.weather_descriptions} and feels like ${body.current.feelslike} degrees.`);
+
+    })
+}
+module.exports = {
+    geocode: geocode,
+    forecast: forecast
+}
